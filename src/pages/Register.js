95fca0e6cfb84945b99192a3adb2a5695ed6433a
soohyang136 <jsh515228@gmail.com>
2022-08-27 23:59:20 +0900

    검색기능

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,15 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import MainHeader from "../components/MainHeader";
 import "../styles/Register.css";
 
 export default function Register(){
+    const [id, setId] = useState();
+    const [password1, setPassword1] = useState();
+    const [password2, setPassword2] = useState();
+    function register(e){
+        e.preventDefault();
+        
+    }
     return (
         <div className="container">
             <MainHeader />
-            <form>
-                <input type={'text'} placeholder="아이디" className="id" />
-                <input type={'password'} placeholder="비밀번호" className="pw"/>
-                <input type={'password'} placeholder="비밀번호 재입력" className="ckpw"/>
+            <form onSubmit={register}>
+                <input type={'text'} placeholder="아이디" className="id" value={id} onChange={(e) => {setId(e.target.value)}} />
+                <input type={'password'} placeholder="비밀번호" className="pw" value={password1} onChange={(e) => {setPassword1(e.target.value)}} />
+                <input type={'password'} placeholder="비밀번호 재입력" className="ckpw"  value={password2} onChange={(e) => {setPassword2(e.target.value)}} />
                 <input type={'submit'} value="회원가입" className="submit" />
             </form>
         </div>
--- src/pages/Search.js
+++ src/pages/Search.js
@@ -5,37 +5,59 @@ import Header from "../components/Header";
 import "../styles/Search.css"
 
 export default function Search(){
-    const [ser_text, setText] = useState();
+    const [isLoading, setIsLoading] = useState(false);
+    const [ser_text, setText] = useState("");
+    const [prText, setPrText] = useState("");
+    const [example, setExample] = useState("");
+    const [mean, setMean] = useState("");
     async function searchWord(){
-        const response = await axios({
-            method: 'post',
-            url: "https://openapi.naver.com/v1/papago/n2mt",
-            data: {
-              "source": 'en',
-              "target": 'ko',
-              "text": ser_text,
-            },
-            headers: {
-                "X-Naver-Client-Id": "jQO21fATTMCC_Or8GoOI",
-                "X-Naver-Client-Secret": "tuk3WCr9a0",
-            },
-          });
+        setIsLoading(true);
+        const response = await axios.get("http://localhost:8080/test?word=" + ser_text);
+        response.data[0] = response.data[0].replace(/\(Edu times\)/g, '');
+        response.data[0] = response.data[0].replace(/\(Kids Edu times\)/g, '');
+        response.data[0] = response.data[0].replace(/\n/g, '<br/>');
+        response.data[1] = response.data[1].replace(/\n/g, '<br/>');
         console.log(response.data);
+        setPrText(ser_text);
+        setText("");
+        setMean(response.data[1]);
+        setExample(response.data[0]);
+        setIsLoading(false);
+    }
+    function enter(e){
+        if(e.key === 'Enter'){
+            searchWord();
+        }
     }
     return (
         <div className="container">
             <Header />
              <Link to="/" >
-                <img src="back.png" className="back" />
+                <img src="back.png" className="back" alt="사진이 없음" />
              </Link>
              <label className="ser">
                 <div className="ser_title">사전 검색</div>
                 <div className="ser_bar">
-                    <input onChange={(e) => {setText(e.target.value);}} value={ser_text} type={'text'}  placeholder="search..." className="ser_input" />
-                    <img onClick={searchWord} src="search.png" className="ser_ck" />
+                    <input onChange={(e) => {setText(e.target.value);}} value={ser_text} type={'text'}  placeholder="search..." className="ser_input" onKeyPress={enter} />
+                    <img onClick={searchWord} src="search.png" className="ser_ck" alt="사진이 없음" />
                 </div>
              </label>
-             <div className="wordMean"></div>
+             <div className="wordMean">
+                
+                {isLoading ? 
+                <div className="loading">로딩중 ...</div>
+                : 
+                <>
+                    <div className="prWord">
+                        <div className="prText">{prText}</div>
+                    </div>
+                    <div className="exandmean">
+                    <div className="mean" dangerouslySetInnerHTML={{__html:mean}}></div>
+                    <div className="example" dangerouslySetInnerHTML={{__html:example}}></div>
+                    </div> 
+                </> 
+                }
+                </div>
         </div>
     );
 }
