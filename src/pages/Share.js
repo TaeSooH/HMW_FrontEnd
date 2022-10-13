dcafa94f7023fdbee735ce8d53179ce16f58c8d2
soohyang136 <jsh515228@gmail.com>
2022-10-13 19:54:29 +0900

    update::세트 공유 백엔드 연결

--- src/pages/Share.js
+++ src/pages/Share.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useState } from 'react'
+import { useEffect } from 'react'
 import Header from '../components/Header'
 import SharedSet from '../components/SharedSet'
 import "../styles/Share.css"
 
 const Share = ({name}) => {
+  const [sharedSets, setSharedSet] = useState([])
+  useEffect(() => {
+    axios.get("https://helpingmemo.ga/wordSet/getSharedWordSet")
+    .then(res => {
+      setSharedSet(res.data);
+    })
+    .catch(err => {
+      console.log(err);
+    })
+  }, []);
   return (
     <>
     <div className="container">
@@ -11,12 +23,9 @@ const Share = ({name}) => {
       <div className='Share_container'>
         
         <div className='fl_box'>
-          <SharedSet />
-          <SharedSet />
-          <SharedSet />
-          <SharedSet />
-          <SharedSet />
-          <SharedSet />
+          {sharedSets.map((data, index) => (
+            <SharedSet id={data.id} title={data.title} word_length={data.word_length} owner={data.owner} />
+          ))}
         </div>
         
       </div>
