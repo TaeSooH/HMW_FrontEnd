febe5cfe8f51da0dd8ec41bc7dfaa29e3805fdac
LLTae2 <taehyun5820@gmail.com>
2022-12-27 16:09:31 +0900

    Migrate::share 컴포넌트 타입스크립트로 변환

---
+++ src/pages/Share.tsx
@@ -0,0 +1,51 @@
+import axios from "axios";
+import React, { useState } from "react";
+import { useEffect } from "react";
+import Header from "../components/Header";
+import SharedSet from "../components/SharedSet";
+import "../styles/Share.css";
+
+interface IProp {
+  name: string;
+}
+interface ISharedSet {
+  id: number;
+  title: string;
+  word_length: number;
+  owner: string;
+}
+
+const Share = (props: IProp) => {
+  const [sharedSets, setSharedSet] = useState([]);
+  useEffect(() => {
+    axios
+      .get("https://helpingmemo.ga/wordSet/getSharedWordSet")
+      .then((res) => {
+        setSharedSet(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+  return (
+    <>
+      <div className="container">
+        <Header username={props.name} />
+        <div className="Share_container">
+          <div className="fl_box">
+            {sharedSets.map((data: ISharedSet, index: number) => (
+              <SharedSet
+                id={data.id}
+                title={data.title}
+                word_length={data.word_length}
+                owner={data.owner}
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Share;
