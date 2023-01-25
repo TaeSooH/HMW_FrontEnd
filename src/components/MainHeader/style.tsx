3cf221e3c5b554b60a1a624ce1869ff49251a355
LLTae2 <taehyun5820@gmail.com>
2023-01-25 17:07:49 +0900

    Refactor::메인 헤더 스타일 코드 리팩토링

---
+++ src/components/MainHeader/style.tsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+export const MainHeader = styled.header`
+  top: 0;
+  width: 100%;
+  height: 12%;
+  background-color: white;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+export const MainLogo = styled.p`
+  font-size: 4vh;
+  color: #1f8bf7;
+  font-weight: bold;
+`;
