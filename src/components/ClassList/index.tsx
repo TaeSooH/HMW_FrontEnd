560e96333bd976e74831a8663c7e1dad181307ad
LLTae2 <taehyun5820@gmail.com>
2023-03-13 00:50:47 +0900

    Create::Class 컴포넌트 생성후 퍼블리싱

---
+++ src/components/ClassList/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import * as S from "./style";
+
+const index = () => {
+  return (
+    <S.ClassBox>
+      <S.TopBox>
+        <S.Title>영어수업임</S.Title>
+      </S.TopBox>
+      <S.SubTitle>누구누구의 수업</S.SubTitle>
+    </S.ClassBox>
+  );
+};
+
+export default index;
---
+++ src/components/ClassList/style.tsx
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+
+export const ClassBox = styled.div`
+  width: 15vw;
+  height: 25vh;
+  background-color: white;
+  margin-left: 8%;
+  margin-top: 2%;
+  box-shadow: 10px 10px 5px 5px rgba(74, 74, 74, 0.3);
+  border-radius: 10px;
+`;
+export const TopBox = styled.div`
+  width: 100%;
+  height: 40%;
+  background-color: #1f8bf7;
+  border-top-left-radius: 10px;
+  border-top-right-radius: 10px;
+`;
+export const Title = styled.p`
+  text-align: center;
+  line-height: 290%;
+  font-size: 30px;
+  color: white;
+  font-weight: 500;
+`;
+export const SubTitle = styled.p`
+  text-align: center;
+  font-size: 20px;
+  line-height: 500%;
+`;
